68ae714a7a5f5c5e0035b8221a30ee5abf995cfc
BCullenward <59399934+BCullenward@users.noreply.github.com>
2024-02-28 09:38:54 -0600

    add alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
         heading="Cities"
         onSelectItem={handleSelectItem}
       />
+      <div className="alert alert-primary">
+        <Alert />
+      </div>
     </div>
   );
 }
